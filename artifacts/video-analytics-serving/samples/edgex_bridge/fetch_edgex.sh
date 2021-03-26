c6fa79939ee2879b182906195f2b46db604e2349
SteveNY-Tibco <chuyang@tibco.com>
2021-03-26 10:21:45 -0400

    Fix scripts too

--- artifacts/video-analytics-serving/samples/edgex_bridge/fetch_edgex.sh
+++ artifacts/video-analytics-serving/samples/edgex_bridge/fetch_edgex.sh
@@ -22,8 +22,8 @@ EDGEX_DIR_REPO_DEVELOPER_SCRIPTS="./developer-scripts/"
 if rm -Rf "$EDGEX_DIR_REPO_DEVELOPER_SCRIPTS"; then
     if git clone -b v1.3.1 https://github.com/edgexfoundry/developer-scripts.git $EDGEX_DIR_REPO_DEVELOPER_SCRIPTS; then
         cd "./$EDGEX_DIR_REPO_DEVELOPER_SCRIPTS/compose-builder/"
-        if make compose no-secty ds-mqtt; then
-            cp ../releases/hanoi/compose-files/docker-compose-hanoi-no-secty.yml $VAS_SOURCE/edgex/docker-compose.yml
+        if make compose no-secty mqtt-broker ds-mqtt ; then
+            cp ../releases/hanoi/compose-files/docker-compose-hanoi-no-secty-mqtt.yml $VAS_SOURCE/edgex/docker-compose.yml
             echo "Successfully fetched repo and produced compose file."
             cd $VAS_SOURCE
         else
@@ -38,7 +38,6 @@ else
     echo "ERROR removing existing $EDGEX_DIR_REPO_DEVELOPER_SCRIPTS folder!"
     exit $?
 fi
-
 
 #######################################################################
 # Fetch EdgeX launch instructions
