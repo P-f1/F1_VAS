1951ca6cbd50a85a1104b5d5d6819f2564a4a6cb
SteveNY-Tibco <chuyang@tibco.com>
2021-03-26 10:00:28 -0400

    Fix the scripts for fetching edgex

--- artifacts/video-analytics-serving/samples/edgex_bridge/clear_edgex.sh
+++ artifacts/video-analytics-serving/samples/edgex_bridge/clear_edgex.sh
@@ -1,6 +1,24 @@
 read -r -p "This will DESTROY all EdgeX data and metadata. Are you sure? [y/N] " response
 
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    docker volume rm edgex_consul-config edgex_consul-data edgex_db-data edgex_log-data
+# Fetch EdgeX launch instructions
+cd $VAS_SOURCE/edgex
+EDGEX_DIR_REPO_DEVELOPER_SCRIPTS="./developer-scripts/"
+if rm -Rf "$EDGEX_DIR_REPO_DEVELOPER_SCRIPTS"; then
+    if git clone -b v1.3.1 https://github.com/edgexfoundry/developer-scripts.git $EDGEX_DIR_REPO_DEVELOPER_SCRIPTS; then
+        cd "./$EDGEX_DIR_REPO_DEVELOPER_SCRIPTS/compose-builder/"
+        if make compose no-secty mqtt-broker ds-mqtt ; then
+            cp ../releases/hanoi/compose-files/docker-compose-hanoi-no-secty-mqtt.yml $VAS_SOURCE/edgex/docker-compose.yml
+            echo "Successfully fetched repo and produced compose file."
+            cd $VAS_SOURCE
+        else
+            echo "ERROR making EdgeX compose file!"
+            exit $?
+        fi
+    else
+        echo "ERROR cloning $EDGEX_DIR_REPO_DEVELOPER_SCRIPTS repo!"
+        exit $?
+    fi
+else
+    echo "ERROR removing existing $EDGEX_DIR_REPO_DEVELOPER_SCRIPTS folder!"
+    exit $?
 fi
--- artifacts/video-analytics-serving/samples/edgex_bridge/fetch_edgex.sh
+++ artifacts/video-analytics-serving/samples/edgex_bridge/fetch_edgex.sh
@@ -18,15 +18,40 @@ docker rm -f dev_mqtt
 
 # Fetch EdgeX launch instructions
 cd $VAS_SOURCE/edgex
-git clone https://github.com/edgexfoundry/developer-scripts.git
-cd ./developer-scripts/releases/nightly-build/compose-files/source
-make compose no-secty mqtt
-cp ../docker-compose-nexus-no-secty-mqtt.yml $VAS_SOURCE/edgex/docker-compose.yml
-cd $VAS_SOURCE
+EDGEX_DIR_REPO_DEVELOPER_SCRIPTS="./developer-scripts/"
+if rm -Rf "$EDGEX_DIR_REPO_DEVELOPER_SCRIPTS"; then
+    if git clone -b v1.3.1 https://github.com/edgexfoundry/developer-scripts.git $EDGEX_DIR_REPO_DEVELOPER_SCRIPTS; then
+        cd "./$EDGEX_DIR_REPO_DEVELOPER_SCRIPTS/compose-builder/"
+        if make compose no-secty ds-mqtt; then
+            cp ../releases/hanoi/compose-files/docker-compose-hanoi-no-secty.yml $VAS_SOURCE/edgex/docker-compose.yml
+            echo "Successfully fetched repo and produced compose file."
+            cd $VAS_SOURCE
+        else
+            echo "ERROR making EdgeX compose file!"
+            exit $?
+        fi
+    else
+        echo "ERROR cloning $EDGEX_DIR_REPO_DEVELOPER_SCRIPTS repo!"
+        exit $?
+    fi
+else
+    echo "ERROR removing existing $EDGEX_DIR_REPO_DEVELOPER_SCRIPTS folder!"
+    exit $?
+fi
 
-echo "Next steps:"
-echo ./docker/build.sh
-echo ./docker/run.sh --dev
-echo python3 samples/edgex_bridge/edgex_bridge.py --topic object_events --generate
-echo "docker-compose up -d"
-echo python3 samples/edgex_bridge/edgex_bridge.py --topic object_events
+
+#######################################################################
+# Fetch EdgeX launch instructions
+#cd $VAS_SOURCE/edgex
+#git clone https://github.com/edgexfoundry/developer-scripts.git
+#cd ./developer-scripts/releases/nightly-build/compose-files
+#make run no-secty
+#cp ../docker-compose-nexus-no-secty-mqtt.yml $VAS_SOURCE/edgex/docker-compose.yml
+#cd $VAS_SOURCE
+
+#echo "Next steps:"
+#echo ./docker/build.sh
+#echo ./docker/run.sh --dev
+#echo python3 samples/edgex_bridge/edgex_bridge.py --topic object_events --generate
+#echo "docker-compose up -d"
+#echo python3 samples/edgex_bridge/edgex_bridge.py --topic object_events
