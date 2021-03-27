37095b694d1d90e84ae6a70f4026a32c637d5a2e
SteveNY-Tibco <chuyang@tibco.com>
2021-03-27 10:13:51 -0400

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,23 +12,40 @@ echo $ProjectID
 echo $Username
 echo $TargetServer
 
-if [ -d "./$DeployType" ] 
-then
-    rm -rf ./$DeployType/* 
-else
-    mkdir -p ./$DeployType
-fi
+################################
+#       Build EdgeX Bridge
+################################
 
-cp -R ../artifacts/docker-compose.yml ./$DeployType
-cp -R ../artifacts/.env.f1 ./$DeployType
+VAS_DIR=../artifacts/video-analytics-serving
 
-cd ./$DeployType
+cd $VAS_DIR
 
-if [[ -v TargetServer ]]; then
-	echo "Deploy Remotely !!"
+pwd
+
+./samples/edgex_bridge/fetch_edgex.sh
+
+
+################################
+#         Deploy VAS
+################################
+#
+#if [ -d "./$DeployType" ] 
+#then
+#    rm -rf ./$DeployType/* 
+#else
+#    mkdir -p ./$DeployType
+#fi
+
+#cp -R ../artifacts/docker-compose.yml ./$DeployType
+#cp -R ../artifacts/.env.f1 ./$DeployType
+
+#cd ./$DeployType
+
+#if [[ -v TargetServer ]]; then
+#	echo "Deploy Remotely !!"
 	#docker-compose --context $DockerContext up -d
-	docker-compose -H "ssh://$Username@$TargetServer" up
-else
-	echo "Deploy Locally !!"
-	docker-compose up
-fi
+#	docker-compose -H "ssh://$Username@$TargetServer" up
+#else
+#	echo "Deploy Locally !!"
+#	docker-compose up
+#fi
